a566c7a33d6c9819864d10e839b6b5142f45cf9b
techraf <techraf@gmail.com>
2018-06-09 22:57:06 +0200

    Specify Python version

--- prepare-conda-environment-for-devops.sh
+++ prepare-conda-environment-for-devops.sh
@@ -8,8 +8,13 @@ env_name="devops"
 # shellcheck disable=SC1091
 source deactivate
 conda remove --yes --name ${env_name} --all
-conda create --yes --name ${env_name}
+conda create --yes --name ${env_name} python=2.7.14
 # shellcheck disable=SC1091
 source activate ${env_name}
 
 source ./include-install-ansible-and-dependencies.sh
+
+#conda install --yes -c ruby-lang ruby=2.5.0
+conda install --yes -c anaconda ruby
+gem install inspec
+#gem install serverspec
